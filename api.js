29a3041f52d4df46272904882058119704f27b02
Alexssandro Gottschalk <alex@gottschalk.com.br>
2018-11-06 11:27:15 -0200

    Conclusão do projeto

--- api.js
+++ api.js
@@ -1,8 +1,9 @@
 const axios = require('axios')
 const baseURL = 'https://como-fazer-ae9a4.firebaseio.com/'
+const authKey = '1LNP7KMFOBH51Z3yA0uOZuFyaj8MgtyZtEkWUb3U'
 
 const list = async (key) => {
-    const content = await axios.get(`${baseURL}${key}.json`)
+    const content = await axios.get(`${baseURL}${key}.json?auth=${authKey}`)
     if (content.data) {
         const objetos = Object.keys(content.data)
             .map(key => {
@@ -12,15 +13,17 @@ const list = async (key) => {
                 }
             })
         return objetos
+    } else {
+        return []
     }
 }
 
 const apagar = async (key, id) => {
-    await axios.delete(`${baseURL}${key}/${id}.json`)
+    await axios.delete(`${baseURL}${key}/${id}.json?auth=${authKey}`)
 }
 
 const get = async (key, id) => {
-    const content = await axios.get(`${baseURL}${key}/${id}.json`)
+    const content = await axios.get(`${baseURL}${key}/${id}.json?auth=${authKey}`)
     if (content.data) {
         return {
             id: id,
@@ -30,11 +33,11 @@ const get = async (key, id) => {
 }
 
 const update = async (key, id, data) => {
-    await axios.put(`${baseURL}${key}/${id}.json`, data)
+    await axios.put(`${baseURL}${key}/${id}.json?auth=${authKey}`, data)
 }
 
 const create = async (key, data) => {
-    await axios.post(`${baseURL}${key}.json`, data)
+    await axios.post(`${baseURL}${key}.json?auth=${authKey}`, data)
 }
 
 module.exports = {
